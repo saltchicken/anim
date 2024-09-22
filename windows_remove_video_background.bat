811d98ab52b6a125d2b51143032165640014ca47
saltchicken <john.eicher89@gmail.com>
2024-09-22 14:07:38 -0700

    Add alphamatting

--- windows_remove_video_background.bat
+++ windows_remove_video_background.bat
@@ -5,7 +5,7 @@ set OUTPUT_FILE=%INPUT_VIDEO%_anim.mkv
 
 mkdir frames
 ffmpeg -i %INPUT_VIDEO% -r 15 -q:v 2 frames/frame_%%04d.png
-rembg p frames %OUTPUT_FOLDER%
+rembg p frames %OUTPUT_FOLDER% alphamatting
 ffmpeg -framerate 15 -i "%OUTPUT_FOLDER%\frame_%%04d.png" -c:v ffv1 -pix_fmt yuva420p -f matroska %OUTPUT_FILE%
 
 rmdir /S /Q frames
