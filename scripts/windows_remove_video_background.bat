1452110fd555a9831a6e2f50518360e84f16c234
saltchicken <john.eicher89@gmail.com>
2024-09-23 18:42:59 -0700

    Update filenames in windows bat

--- scripts/windows_remove_video_background.bat
+++ scripts/windows_remove_video_background.bat
@@ -1,7 +1,8 @@
 @echo off
 set INPUT_VIDEO=%1
 set OUTPUT_FOLDER=processed_%INPUT_VIDEO%
-set OUTPUT_FILE=%INPUT_VIDEO%_anim.mkv
+set INPUT_VIDEO_NO_EXTENSION=%~n1
+set OUTPUT_FILE=%INPUT_VIDEO_NO_EXTENSION%_anim.mkv
 
 mkdir frames
 ffmpeg -i %INPUT_VIDEO% -r 15 -q:v 2 frames/frame_%%04d.png
